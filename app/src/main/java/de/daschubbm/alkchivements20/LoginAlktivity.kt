c9f2823ed232d500d61d914d68f3ef1012ea436c
jjoonnii <jonathan.seibold@stud.uni-regensburg.de>
2017-06-16 19:11:41 +0200

    Initial commit

---
+++ app/src/main/java/de/daschubbm/alkchivements20/LoginAlktivity.kt
@@ -0,0 +1,26 @@
+package de.daschubbm.alkchivements20
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.alktivity_login.*
+
+class LoginAlktivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.alktivity_login)
+    }
+
+    fun login(view: View) {
+        val name = nameField.text.toString()
+        if (name.matches(Regex("\\p{Lu}\\p{Ll}{2,26}( \\p{Lu}\\p{Ll}{2,26})?")) && name != "Anthony Modeste") {
+            val hansl = Intent(this, MainAlktivity::class.java)
+            startActivity(hansl)
+        } else {
+            Toast.makeText(this, "Gib an gscheidn Nam ein, Affngsicht!", Toast.LENGTH_LONG).show()
+        }
+    }
+}
---
+++ app/src/main/java/de/daschubbm/alkchivements20/MainAlktivity.kt
@@ -0,0 +1,12 @@
+package de.daschubbm.alkchivements20
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainAlktivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.alktivity_main)
+    }
+}
